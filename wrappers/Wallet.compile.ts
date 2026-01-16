713a415f3ae903f15955a38e225ed669892d0ac1
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-01-16 08:57:20 +0300

    feat: add jetton logic

--- wrappers/Wallet.compile.ts
+++ wrappers/Wallet.compile.ts
@@ -5,5 +5,4 @@ export const compile: CompilerConfig = {
     entrypoint: 'contracts/wallet.tolk',
     withStackComments: true, // Fift output will contain comments, if you wish to debug its output
     withSrcLineComments: true, // Fift output will contain .tolk lines as comments
-    experimentalOptions: '', // you can pass experimental compiler options here
 };
